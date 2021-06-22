eff02e16001317d17e287cb9b624ee322360e210
Julius-R <julius.robinson95@yahoo.com>
2021-06-22 11:10:21 -0600

    added libraries for rating and icons

---
+++ src/components/PhotoGrid.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+// const data = [
+//     {
+//         name:"Jerry's Flapjack Highstack",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quam odit illo saepe.",
+//         rating: 5,
+//         url: require("../../assets/flapjacks.jpg"),
+//         photographer: "Photo by Unknown from Pexels"
+//     },
+//     {
+//         name:"Rusty's Last Meal",
+//         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, recusandae!",
+//         rating: 5,
+//         url: require("../../assets/burger.jpg"),
+//         photographer: "Photo by Jonathan Borba from Pexels"
+//     },
+//     {
+//         name:"High Naan Wheat Bread",
+//         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, recusandae!",
+//         rating: 3,
+//         url: require("../../assets/drywheatbread.jpg"),
+//         photographer: "Photo by Mariana Kurnyk from Pexels"
+//     },
+//     {
+//         name:"Slim Bob's Shishkebabs",
+//         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, recusandae!",
+//         rating: 4,
+//         url: require("../../assets/shishkebabs.jpg"),
+//         photographer: "Photo by Unknown from Pexels"
+//     },
+//     {
+//         name:"Suzy's Afternoon Salad",
+//         description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, recusandae!",
+//         rating: 4,
+//         url: require("../../assets/salad.jpg"),
+//         photographer: "Photo by julie aagaard from Pexels"
+//     },
+// ]
+
+export default function Photogrid() {
+    return (
+        <section className="grid">
+            <div>a</div>
+            <div>d</div>
+            <div>f</div>
+            <div>g</div>
+            <div>y</div>
+        </section>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.sass';
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
